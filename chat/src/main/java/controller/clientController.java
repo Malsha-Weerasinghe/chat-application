5e5e9b878d12daeaf718832f821fc2f222a0d8cd
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-07-02 21:14:18 +0530

    create run method

--- chat/src/main/java/controller/clientController.java
+++ chat/src/main/java/controller/clientController.java
@@ -1,20 +1,35 @@
 package controller;
 
-import javafx.event.ActionEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 
-import java.awt.*;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.Socket;
 
 public class clientController extends Thread{
+
    public AnchorPane emojiPane;
    public Label lblName;
    public TextField messageField;
    public ScrollPane messageShowField;
-   public VBox vBox;
+   public javafx.scene.layout.VBox vBox;
 
    BufferedReader reader;
    PrintWriter writer;
@@ -37,6 +52,121 @@ public class clientController extends Thread{
          e.printStackTrace();
       }
       emojiPane.setVisible(false);
+   }
+
+   @Override
+   public void run(){
+      try {
+         while (true) {
+
+            String message = reader.readLine();
+            String[] tokens = message.split(" ");
+            String command = tokens[0];
+
+            StringBuilder fullMessage = new StringBuilder();
+            for (int i = 1; i < tokens.length; i++) {
+               fullMessage.append(tokens[i] + " ");
+            }
+
+            String[] msgArray = message.split(" ");
+            String s = "";
+            for (int i = 0; i < msgArray.length - 1; i++) {
+               s += msgArray[i + 1] + " ";
+            }
+
+            Text text = new Text(s);
+            String firstLetter = "";
+
+            if (s.length() > 3) {
+               firstLetter = s.substring(0, 3);
+            }
+
+            if (firstLetter.equalsIgnoreCase("img")) {
+
+               s = s.substring(3, s.length() - 1);
+
+               File file = new File(s);
+               Image image = new Image(file.toURI().toString());
+
+               ImageView imageView = new ImageView(image);
+               imageView.setFitHeight(150);
+               imageView.setFitWidth(200);
+
+               final HBox hbox = new HBox(10);
+               hbox.setAlignment(Pos.BOTTOM_RIGHT);
+
+               if (!command.equalsIgnoreCase(lblName.getText())) {
+
+                  vBox.setAlignment(Pos.TOP_LEFT);
+                  hbox.setAlignment(Pos.CENTER_LEFT);
+
+                  Text textNew = new Text("  " + command + " :");
+                  hbox.getChildren().add(textNew);
+                  hbox.getChildren().add(imageView);
+
+               } else {
+                  hbox.setAlignment(Pos.BOTTOM_RIGHT);
+                  hbox.getChildren().add(imageView);
+                  Text textNew = new Text("");
+                  hbox.getChildren().add(textNew);
+               }
+
+
+               Platform.runLater(() -> vBox.getChildren().addAll(hbox));
+            } else {
+               TextFlow textFlow = new TextFlow();
+
+               if (!command.equalsIgnoreCase(lblName.getText() + ":")) {
+
+                  Text textName = new Text(command + " ");
+                  textName.getStyleClass().add("textName");
+                  textFlow.getChildren().add(textName);
+
+                  textFlow.setStyle("-fx-color: rgb(239,242,255);" +
+                          "-fx-background-color: rgb(95,48,224);" +
+                          " -fx-background-radius: 30px");
+                  textFlow.setPadding(new Insets(3, 10, 3, 10));
+
+               }
+
+               textFlow.getChildren().add(text);
+               textFlow.setMaxWidth(200);
+
+               TextFlow flow = new TextFlow(textFlow);
+               final HBox hBox = new HBox(12);
+
+               if (!command.equalsIgnoreCase(lblName.getText() + ":")) {
+
+                  vBox.setAlignment(Pos.TOP_LEFT);
+                  hBox.setAlignment(Pos.CENTER_LEFT);
+                  hBox.getChildren().add(flow);
+
+               } else {
+
+                  Text text1 = new Text(fullMessage + " ");
+                  TextFlow flow1 = new TextFlow(text1);
+                  hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                  hBox.getChildren().add(flow1);
+                  hBox.setPadding(new Insets(2, 5, 2, 10));
+
+
+                  flow1.setStyle("-fx-color: rgb(239,242,255);" +
+                          "-fx-background-color: rgb(92,150,231);" +
+                          "-fx-background-radius: 30px");
+                  flow1.setPadding(new Insets(3, 10, 3, 10));
+               }
+
+               Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+
+
+            }
+         }
+      }catch (Exception e){
+         e.printStackTrace();
+      }
+
+
+
    }
 
    public void messageOnAction(ActionEvent event) {

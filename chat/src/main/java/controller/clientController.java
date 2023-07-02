95071ac551ce1b3275dbac078491a58bb8d1e32f
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-07-02 16:11:55 +0530

    client Form

---
+++ chat/src/main/java/controller/clientController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
+
+public class clientController {
+   public AnchorPane emojiPane;
+   public Label lblName;
+   public TextField messageField;
+   public ScrollPane messageShowField;
+   public VBox vBox;
+
+   public void messageOnAction(ActionEvent event) {
+
+    }
+}

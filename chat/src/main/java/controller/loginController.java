349e1c9573fa5671fe64aaf06e7a40ada006d222
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-07-02 16:21:36 +0530

    login controller

--- chat/src/main/java/controller/loginController.java
+++ chat/src/main/java/controller/loginController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class loginController {
+
+    public TextField nameField;
+    static String inputName;
+
+    public void nameOnAction(ActionEvent event) throws IOException {
+
+        inputName = nameField.getText();
+        nameField.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene((Parent) FXMLLoader.load(loginController.class.getResource("/view/clientForm.fxml"))));
+    }
 }
